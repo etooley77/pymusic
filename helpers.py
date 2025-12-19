ea10ab5316b720b803b924ffdd254a110c208729
etooley77 <tooleye77@gmail.com>
2025-12-19 09:06:02 -0500

    Remove unnecessary helper function

--- helpers.py
+++
@@ -1,8 +0,0 @@
-from math import sqrt
-
-def distance(point, point2):
-	x_sq = pow(point2[0] - point[0], 2)
-	y_sq = pow(point2[1] - point[1], 2)
-
-	distance = sqrt(abs(x_sq + y_sq))
-	return distance
